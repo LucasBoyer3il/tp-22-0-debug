c5d2d428435e00e5aa7bd13d55d056e1a81c1e4e
boyerl <boyerl@3il.fr>
2023-11-22 11:43:40 +0100

    Correction des bugs dans le fichier Main.java

--- Main.java
+++ Main.java
@@ -2,10 +2,11 @@ public class Main {
     public static void main(String[] args) {
         // Problème 1
         int[] numbers = {1, 2, 3, 4, 5};
-        for (int i = 0; i <= numbers.length; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             System.out.println("Number at index " + i + ": " + numbers[i]);
         }
-
+        // Solution : i < numbers.length et non <=
+        
         // Problème 2
         double result = calculateValue(5, 0);
         System.out.println("Result: " + result);
@@ -14,11 +15,11 @@ public class Main {
         int count = 10;
         while (count > 0) {
             System.out.println("Count: " + count);
-            count += 2;
+            count -= 2;
         }
 
         // Problème 4
-        String message = "Bonjour";
+        String message = "Bonjour utilisateur";
         System.out.println("Longueur du message : " + message.length());
         char character = message.charAt(10);
         System.out.println("Caractère à l'indice 10 : " + character);
@@ -26,16 +27,22 @@ public class Main {
         // Problème 5
         String str1 = "Hello";
         String str2 = new String("Hello");
-        System.out.println("Comparaison de chaînes : " + (str1 == str2));
+        System.out.println("Comparaison de chaînes : " + (str1.equals(str2)));
 
         // Problème 6
-        Main mainObj = null;
+        Main mainObj = new Main();
         mainObj.method();
     }
 
     // Méthode pour le problème de division par zéro
     public static int calculateValue(int a, int b) {
-        return a / b;
+    	int resultat;
+    	if (b == 0) {
+    		resultat = 0;
+    	} else {
+    		resultat = a / b;
+    	}
+        return resultat;
     }
 
     // Méthode pour le problème d'utilisation incorrecte d'une méthode de classe
